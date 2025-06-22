6d5751ca876514834d67d0e689da22d333a27efc
huanghaiqingit <103879245+huanghaiqingit@users.noreply.github.com>
2025-06-22 13:19:20 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -34,8 +34,8 @@ CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的�
 USER_NAME = "kkddytd3"         # 用户名
 UUID = "a91b59b6-ade3-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = ""                   # 域名，留空则自动获取
-CF_TOKEN = ""                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "abs.bolgkuyz.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiYzU4N2M0NzBmYWNmYmJhMzVlNzJkYzRjYzAxZThhMDIiLCJ0IjoiYzdiZGEwNGEtOWUzMS00MmNhLTk2MGQtNjZhZDQ5MDQyZDBkIiwicyI6Ik1qUTBPV00zTW1VdE5UVXdOQzAwTmpFM0xXRXdNR1V0WVRFeVpHSTBaV0k1WWpreSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
